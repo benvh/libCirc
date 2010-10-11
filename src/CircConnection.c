fb550fce4d22e4d073b8d7acb3991aadf1126b5e
benvh <ben.vanhoutven@gmail.com>
2010-10-11 21:03:36 +0200

    fixed some typos

--- src/CircConnection.c
+++ src/CircConnection.c
@@ -1,9 +1,11 @@
 #include "CircConnection.h"
 #include <stdlib.h>
+#include <gio/gio.h>
 
 struct _CircConnection
 {
-    ConnectionStatus        status;
+    CircConnectionStatus    status;
+    CircIdentity*           identity;
     
     GSocketConnectable*     sock_addr;
     GSocketClient*          sock_clie;
@@ -13,9 +15,10 @@ struct _CircConnection
 
 
 /* Private Functions */
-CircConnection* circ_connection_init(CircConnection* self, const gchar* host, int port)
+CircConnection* circ_connection_init(CircConnection* self, CircIdentity* identity, const gchar* host, int port)
 {
     self->status = STATUS_DISCONNECTED;
+    self->identity = identity;
     
     self->sock_addr = g_network_address_new(host, port);
     self->sock_clie = g_socket_client_new();
@@ -25,7 +28,7 @@ CircConnection* circ_connection_init(CircConnection* self, const gchar* host, in
 }
 
 /* Public Functions */
-CircConnection* circ_connection_new(const gchar* host, int port)
+CircConnection* circ_connection_new(CircIdentity* identity, const gchar* host, int port)
 {
-    return circ_connection_init( (CircConnection*)malloc(sizeof(CircConnetion)), host, port);
+    return circ_connection_init( (CircConnection*)malloc(sizeof(CircConnection)), identity, host, port);
 }
