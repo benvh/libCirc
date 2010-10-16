1f9b42573d4f8c0dc2a532c68b1598121ed57e55
benvh <ben.vanhoutven@gmail.com>
2010-10-16 13:55:09 +0200

    'Merged' CircConncetion_in.c from testing branch (commit 3cfb759bfc5b37db741e607d974a26a3233d08fc)

--- src/CircConnection_in.c
+++ src/CircConnection_in.c
@@ -6,8 +6,9 @@ extern void circ_connection_update_status(CircConnection*, CircConnectionStatus)
 extern GHashTable* circ_connection_get_callbacks(CircConnection*);
 extern void out_pong(CircConnection*, const gchar*);
 
-void in_numeric_reply(CircConnection* self, int num_reply, const gchar* params);
-void in_message(CircConnection* self, const gchar* from, const gchar* channel, const gchar* text);
+void in_numeric_reply(CircConnection *self, int num_reply, const gchar *params);
+void in_message(CircConnection *self, const gchar *from, const gchar *channel, const gchar *text);
+void in_notice(CircConnection *self, const gchar *from, const gchar *text);
 
 
 
@@ -37,14 +38,18 @@ void in_parse_message(CircConnection* self, const gchar* raw_message)
     if(num_reply != 0) in_numeric_reply(self, num_reply, raw_split0[1]);
     
     //no num_reply
+    gchar** nick_host = g_strsplit(raw_split1[0], "!", 0);
+    
     if(g_strcmp0(raw_split1[1], "PRIVMSG") == 0)
     {
-		gchar** nick_host = g_strsplit(raw_split1[0], "!", 0);
 		in_message(self, nick_host[0]+1, raw_split1[2], raw_split0[1]);
-		
-		g_strfreev(nick_host);
+    }
+    else if(g_strcmp0(raw_split1[1], "NOTICE") == 0)
+    {
+ 		in_notice(self, nick_host[0]+1, raw_split0[1]);
     }
     
+    g_strfreev(nick_host);
     g_strfreev(raw_split0);
     g_strfreev(raw_split1);
 }
@@ -80,6 +85,17 @@ void in_message(CircConnection* self, const gchar* from, const gchar* channel, c
 	{
 		gdk_threads_enter();
 		callback(self, from, channel, text);
+		gdk_threads_leave();
+	}
+}
+
+void in_notice(CircConnection *self, const gchar *from, const gchar *text)
+{
+	CircEventCallback callback = g_hash_table_lookup( circ_connection_get_callbacks(self), "notice-received");
+	if(callback)
+	{
+		gdk_threads_enter();
+		callback(self, from, text);
 		gdk_threads_leave();
 	}
 }
